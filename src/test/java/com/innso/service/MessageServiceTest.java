9782bce2eb8b94c657045a452a7d14514e4b86ff
brabii <collectorsgarbages@gmail.com>
2021-05-24 16:54:06 +0200

    ajout test should return message when message founded

--- src/test/java/com/innso/service/MessageServiceTest.java
+++ src/test/java/com/innso/service/MessageServiceTest.java
@@ -1,17 +1,39 @@
 package com.innso.service;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.innso.exception.MessageNotFoundException;
+import com.innso.model.Message;
 
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
 public class MessageServiceTest {
 
-	private static final long MESSAGE_ID_NO_EXISTS = 2;
+	private static final long MESSAGE_NOT_EXISTS_ID = 2;
+	private static final long MESSAGE_EXISTS_ID = 1;
+	private MessageService msgService;
+
+	@Before
+	public void initialise() {
+		msgService = new MessageServiceImpl();
+	}
 
 	@Test(expected = MessageNotFoundException.class)
 	public void shouldThrowExceptionWhenMessageNotFound() throws MessageNotFoundException {
-		MessageService msgService = new MessageServiceImpl();
-		msgService.findById(MESSAGE_ID_NO_EXISTS);
+		msgService = new MessageServiceImpl();
+		msgService.findById(MESSAGE_NOT_EXISTS_ID);
 	}
-	
+
+	@Test
+	public void shouldReturnMessageWhenMessageFounded() throws MessageNotFoundException {
+		msgService = new MessageServiceImpl();
+		Message message = msgService.findById(MESSAGE_EXISTS_ID);
+		assertTrue(!message.equals(null));
+	}
+
 }
